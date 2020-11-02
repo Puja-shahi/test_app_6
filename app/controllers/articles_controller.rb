0865e18a5e55a9e46bf62f63ff337d724635b630
Puja shahi <puja78300@gmail.com>
2020-11-02 12:50:00 +0000

    added edit and update action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,32 @@ class ArticlesController < ApplicationController
     def index
         @articles = Article.all
     end    
+    
+    def new
+        @art = Article.new
+    end
+    
+    def edit
+        @article = Article.find(params[:id])
+    end    
+    
+    def create
+        @art = Article.new(params.require(:article).permit(:title, :description))
+        if @art.save  
+            flash[:notice] = "Article was created successfully"
+            redirect_to @art
+        else
+            render 'new'
+        end     
+    end     
+    
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title, :description))
+           flash[:notice] = "Article was updated successfully"
+           redirect_to @article
+        else
+            render 'edit'
+        end    
+    end    
 end 
